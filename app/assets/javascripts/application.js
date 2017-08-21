86d1f5feefd8fb8551163d3b8013c96b099d5d2d
Michael Klishevich <busation@gmail.com>
2017-08-21 08:56:38 +0300

    active menu items

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,5 @@
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require turbolinks
+//= require_self
 //= require_tree .
--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,7 +1,17 @@
-// $(function() {
-//   $(".navbar-nav a").on("click", function(){
-//     alert('hi!');
-//     $(".navbar-nav").find(".active").removeClass("active");
-//     $(this).parent().addClass("active");
-//   });
-// });
+var updateActiveMenu = function(){
+  $(document).ready(function(){
+    // $(".navbar-nav").find(".active").removeClass("active");
+    const pathname = window.location.pathname;
+    if ( pathname === '/courses' ) {
+      $("#courses").addClass("active");
+    }
+    if ( pathname === '/mentors' ) {
+      $("#mentors").addClass("active");
+    }
+    if ( pathname === '/faq' ) {
+      $("#faq").addClass("active");
+    }
+  });
+};
+
+$(document).on('turbolinks:load', updateActiveMenu);
