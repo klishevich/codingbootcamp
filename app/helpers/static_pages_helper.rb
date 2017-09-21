a262718608ad1ab1fba4d0a7b176e9cd40a7d184
Michael Klishevich <busation@gmail.com>
2017-09-21 09:17:22 +0300

    3 courses now

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,8 @@
 module StaticPagesHelper
+  def course_info(title, text)
+    content_tag(:div, class: 'row') do
+      content_tag(:div, content_tag(:b, "#{title}:"), class: 'col-lg-3') +
+      content_tag(:div, text, class: 'col-lg-9')
+    end
+  end
 end
