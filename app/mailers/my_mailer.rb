11f5bb6e428818feae93b58814b46b5f6bdbbaf2
Michael Klishevich <busation@gmail.com>
2017-10-17 23:22:56 +0300

    fix

--- app/mailers/my_mailer.rb
+++ app/mailers/my_mailer.rb
@@ -1,7 +1,7 @@
 class MyMailer < ApplicationMailer
   default from: 'info@codingbootcamp.ru'
   def new_registration(user)
-    mail(to: 'busation@gmail.com',
+    mail(to: 'info@codingbootcamp.ru',
          subject: 'New Registration Coding Bootcamp',
          body: "User #{user.id},email #{user.email}, full_name #{user.full_name}, preferred_name #{user.preferred_name}, phone #{user.phone}, city #{user.city}, comment #{user.comment}")
   end
