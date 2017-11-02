9ae707f17ed7efa4d7cfdafbaee4f8c90a62c2fd
Michael Klishevich <busation@gmail.com>
2017-11-02 09:49:09 +0300

    courses and lessons finished

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,2 +1,45 @@
 class LessonsController < ApplicationController
+  before_action :auth_mentor
+  before_action :set_course, only: [:create, :new, :edit, :update, :show]
+
+  def new
+    @lesson = Lesson.new
+  end
+
+  def create
+    @lesson = @course.lessons.build(lesson_params)
+    if @lesson.save
+      flash[:notice] = t(:created_successfuly)
+      redirect_to course_lesson_path(@course, @lesson)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @lesson = @course.lessons.find(params[:id])
+  end
+
+  def update
+    @lesson = @course.lessons.find(params[:id])
+    if @lesson.update_attributes(lesson_params)
+      flash[:notice] = t(:saved_successfuly)
+      redirect_to course_lesson_path(@course, @lesson)
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def set_course
+    @course = Course.find(params[:course_id])
+  end
+
+  def lesson_params
+    params.require(:lesson).permit(:name, :video_desc, :task_desc, :reading_desc, :comment_desc)
+  end
+
 end
