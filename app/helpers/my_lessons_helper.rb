d67d7f319ace965acd36224e0e808cf1b6d40237
Michael Klishevich <busation@gmail.com>
2017-12-14 09:14:17 +0300

    last course path

--- app/helpers/my_lessons_helper.rb
+++ app/helpers/my_lessons_helper.rb
@@ -20,7 +20,7 @@ module MyLessonsHelper
     end
   end
 
-  def link_to_last_lesson(my_course)
+  def link_to_last_my_lesson(my_course)
     lesson_name = my_course.my_lessons.last&.lesson&.name
     if !lesson_name
       return nil
@@ -28,6 +28,16 @@ module MyLessonsHelper
       my_course_id = my_course.id
       my_lesson_id = my_course.my_lessons.last.id
       return link_to lesson_name, my_course_my_lesson_path(my_course_id, my_lesson_id)
+    end
+  end
+
+  def last_course_helper_path
+    last_course = Course.last
+    if !last_course
+      return '#'
+    else
+      course_id = last_course.id
+      return course_path(course_id)
     end
   end
 end
