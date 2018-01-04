8b2fc1d6fa0bc719df36d6b021e3cc6bb6f0fa2e
Michael Klishevich <busation@gmail.com>
2018-01-04 21:23:39 +0300

    Coding Bootcamp Ru

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = '"Boston-Moscow Coding Bootcamp" <info@codingbootcamp.ru>'
+  config.mailer_sender = '"Coding Bootcamp Ru" <info@codingbootcamp.ru>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
